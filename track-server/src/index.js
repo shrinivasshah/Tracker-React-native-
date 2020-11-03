ac8c212f3b5fe02c6c89f548eec5bcd4be88ddf3
Shrinivas <shrinivas@Shrinivass-MacBook-Pro.local>
2020-11-03 19:04:51 +0530

    initial commit auth backend done

---
+++ track-server/src/index.js
@@ -0,0 +1,30 @@
+require("./models/User");
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const requireAuth = require("./middlewares/requireAuth");
+const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+const mongoUri =
+  "mongodb+srv://shri:hachangbong@tracker.tkrtq.mongodb.net/emaily?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connnected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
