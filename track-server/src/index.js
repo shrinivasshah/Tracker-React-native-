c931d30e9c5d530aa67dba7dd8a93225658fafb6
Shrinivas <shrinivas@Shrinivass-MacBook-Pro.local>
2020-11-04 19:27:22 +0530

    added tracks

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,12 +1,15 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const requireAuth = require("./middlewares/requireAuth");
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 const mongoUri =
   "mongodb+srv://shri:hachangbong@tracker.tkrtq.mongodb.net/emaily?retryWrites=true&w=majority";
 mongoose.connect(mongoUri, {
